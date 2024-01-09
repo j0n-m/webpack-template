6ac15c69a203f25626ee271be31a79e0118d9b3d
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2024-01-09 10:07:03 -0600

    fixed js entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './foo.js',
+  entry: './src/index.js',
   devtool: 'source-map',
   devServer: {
     static: './dist',
